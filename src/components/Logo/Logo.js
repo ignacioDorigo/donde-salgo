17c788d80ec976a3e6596cbda41e9cb4117251b1
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-05-16 01:21:49 -0300

    nav listo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Logo.css";
+
+export default function Logo() {
+  return (
+    <div className="logo">
+      <img className="logo__img" src="logo.png" alt="Logo donde salgo" title="Logo Donde Salgo" />
+    </div>
+  );
+}
---
+++ src/components/Salidas/Salidas.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Salidas() {
+  return <div>Salidas</div>;
+}
