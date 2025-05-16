6b15c1a5ebb1dedb742aa67ed0726eb3f5efe389
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-05-16 02:22:55 -0300

    categorias listo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Categorias from "./components/Categorias/Categorias";
 import Main from "./components/Main/Main";
 import Navbar from "./components/Navbar/Navbar";
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Main />
+      <Categorias />
     </div>
   );
 }
